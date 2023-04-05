e0e0d5acc9e975f5cc65fa7829e24894eec4ee15
Bozai Cosmin <cosminbozai@outlook.com>
2023-04-05 19:07:00 +0100

    Added hero and technologies sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGray: "#242424",
+        lightGray: "#D9D9D9",
+        paleGreen: "#4EE1A0",
+        background: "#151515",
+      },
+    },
   },
   plugins: [],
 };
